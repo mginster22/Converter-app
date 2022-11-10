3d6f6d5331f7169c1c1fe0b8edd7284c03ca6938
Vlad Korochanskiy <mginster234942@gmai.com>
2022-11-10 11:50:52 +0200

    add constsants for price and currency

--- src/components/hooks/useInput.js
+++ src/components/hooks/useInput.js
@@ -1,33 +1,33 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getAllCurrencies } from "../../api";
 import { setRates } from "../../reducer/curencySlice";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { CONSTANTS } from "../../constants";
+const { BASE_CURRENCY_IN, BASE_CURRENCY_TO, BASE_PRICE_IN, BASE_PRICE_TO } =
+  CONSTANTS;
 export const useInput = () => {
   const dispatch = useDispatch();
 
   const formatNum = (num) => num.toFixed(4);
   const { rates } = useSelector((state) => state);
 
-  const [price1, setPrice1] = useState(1);
-  const [currency1, setCurrency1] = useState("USD");
-  const [price2, setPrice2] = useState(36.8046);
-  const [currency2, setCurrency2] = useState("UAH");
+  const [price1, setPrice1] = useState(BASE_PRICE_TO);
+  const [currency1, setCurrency1] = useState(BASE_CURRENCY_TO);
+  const [price2, setPrice2] = useState(BASE_PRICE_IN);
+  const [currency2, setCurrency2] = useState(BASE_CURRENCY_IN);
 
-   useEffect(() => {
+  useEffect(() => {
     getAllCurrencies().then(({ data }) => {
       dispatch(setRates(data.rates));
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
 
   console.log(rates[currency2]);
   const handlerPriceChange1 = (price1) => {
     setPrice2(formatNum((price1 * rates[currency2]) / rates[currency1]));
     setPrice1(price1);
   };
-
-  
 
   const handlerCurrency1 = (currency1) => {
     setPrice2(formatNum((price1 * rates[currency2]) / rates[currency1]));
@@ -46,7 +46,8 @@ export const useInput = () => {
   return {
     price1,
     currency1,
-    price2,currency2,
+    price2,
+    currency2,
     handlerPriceChange1,
     handlerCurrency1,
     handlerPriceChange2,
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 export const CONSTANTS = {
   BASE_URL:
     "https://api.apilayer.com/exchangerates_data/latest?apikey=WazAa9cpOqunVaTNZ5rwfJ2qtU2P6vfc",
-    
+  BASE_CURRENCY_TO:'USD',  
+  BASE_CURRENCY_IN:'UAH',  
+  BASE_PRICE_TO:1,  
+  BASE_PRICE_IN:36.8046,  
 };
